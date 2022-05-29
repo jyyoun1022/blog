3d7293123c8222682b32640f7fa84e7e30175588
younjaeyeal <jyyoun1021@gmail.com>
2022-05-30 03:07:43 +0900

    더미데이터 테스트

--- src/main/java/org/codej/blog/test/DummyControllerTest.java
+++ src/main/java/org/codej/blog/test/DummyControllerTest.java
@@ -4,8 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.codej.blog.model.RoleType;
 import org.codej.blog.model.User;
 import org.codej.blog.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController
@@ -13,6 +18,34 @@ import java.util.function.Supplier;
 public class DummyControllerTest {
 
     private final UserRepository userRepository;
+
+    @GetMapping("/dummy/list")
+    public List<User> userList(){
+        return userRepository.findAll();
+    }
+    @GetMapping("/dummy/list/page")
+    public List<User> pageList(@PageableDefault(size = 2,sort = "id",direction = Sort.Direction.DESC)Pageable pageable){
+        Page<User> users = userRepository.findAll(pageable);
+        List<User> user = users.getContent();
+        return user;
+    }
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable Long id,@RequestBody User requestUser){
+        System.out.println("id = "+id);
+        System.out.println("password = "+requestUser.getPassword());
+        System.out.println("email =" +requestUser.getEmail());
+        requestUser.setId(id);
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        userRepository.save(user);
+        return null;
+
+    }
+
 
     @PostMapping("/dummy/join")
     public String join(User user){
@@ -29,11 +62,16 @@ public class DummyControllerTest {
     @GetMapping("/dummy/user/{id}")
     public User detail(@PathVariable Long id){
 
-        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
-            @Override
-            public IllegalArgumentException get() {
-                return new IllegalArgumentException("해당 유저는 없습니다. id= " + id);
-            }
+//        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+//            @Override
+//            public IllegalArgumentException get() {
+//                return new IllegalArgumentException("해당 유저는 없습니다. id= " + id);
+//            }
+//        });
+
+        //람다식
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("해당 사용자는 없습니다 id = " + id);
         });
         return user;
     }
