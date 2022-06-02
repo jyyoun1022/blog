b3fe24e8a9f343e53ab9f1e4a1f661de39d18ab3
younjaeyeal <jyyoun1021@gmail.com>
2022-06-03 04:12:38 +0900

    mvc설정

--- src/main/java/org/codej/blog/configuration/WebMvcConfig.java
+++ src/main/java/org/codej/blog/configuration/WebMvcConfig.java
@@ -1,8 +1,16 @@
-package org.codej.blog.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-}
+//package org.codej.blog.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+////    @Override
+////    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        registry
+////                .addResourceHandler("/resources/**")
+////                .addResourceLocations("classpath:/resources/");
+////    }
+//}
