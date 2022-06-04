3e03fedaf74495348289e1fc3d1f4066f901db9d
younjaeyeal <jyyoun1021@gmail.com>
2022-06-05 03:43:22 +0900

    글쓰기 save 메서드

--- src/main/java/org/codej/blog/controller/BoardController.java
+++ src/main/java/org/codej/blog/controller/BoardController.java
@@ -1,14 +1,26 @@
 package org.codej.blog.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.codej.blog.configuration.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
+
 
     @GetMapping({"/",""})
     public String index(){
         return "index";
     }
+    @GetMapping("/board/save")
+    public String saveForm(){
+        return "/board/save";
+    }
+
 
 }
