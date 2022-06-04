4bec4b460d861944b14fa4ccb0099c9b6391caf1
younjaeyeal <jyyoun1021@gmail.com>
2022-06-05 01:52:48 +0900

    configure(AuthenticationMangerBuilder auth) 오버라이드

--- src/main/java/org/codej/blog/configuration/SecurityConfig.java
+++ src/main/java/org/codej/blog/configuration/SecurityConfig.java
@@ -1,7 +1,10 @@
 package org.codej.blog.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.codej.blog.configuration.auth.PrincipalDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,26 +14,38 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration//빈등록(IOC 관리)
 @EnableWebSecurity//시큐리티 필터가 등록이 된다.
-@EnableGlobalMethodSecurity(prePostEnabled = true)//특정 주소로 접근을 하면 권한 및 인증을 미리 체
+@EnableGlobalMethodSecurity(prePostEnabled = true)//특정 주소로 접근을 하면 권한 및 인증을 미리 체크
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PrincipalDetailService principalDetailService;
 
     @Bean
     public BCryptPasswordEncoder encodePWD(){
         return new BCryptPasswordEncoder();
     }
+    //시큐리티가 대신 로그인을 해주는데 password를 가로채기를 하는데
+    //해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야
+    //같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http
+                http
                     .csrf().disable() //csrf 토큰 비활성화
                 .authorizeRequests()
                     .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/images/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
-                        .and()
+                .and()
                     .formLogin()
-                    .loginPage("/blog/user/auth/login");
+                    .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")   //스프링 시큐리티가 해당 주소로 로그인을 가로채서 대신 로그인.
+                        .defaultSuccessUrl("/");//실패할때는.failureUrl()
 
 
 
