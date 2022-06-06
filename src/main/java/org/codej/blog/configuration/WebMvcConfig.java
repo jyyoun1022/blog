aa8ff7e955d759b1193b851854c0c8b1d7476fae
younjaeyeal <jyyoun1021@gmail.com>
2022-06-06 16:59:52 +0900

    시큐리티 설정 클래스

--- src/main/java/org/codej/blog/configuration/WebMvcConfig.java
+++ src/main/java/org/codej/blog/configuration/WebMvcConfig.java
@@ -1,7 +1,9 @@
 //package org.codej.blog.configuration;
 //
+//import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
 //@Configuration
@@ -13,4 +15,10 @@
 ////                .addResourceHandler("/resources/**")
 ////                .addResourceLocations("classpath:/resources/");
 ////    }
+//
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        ThymeleafAutoConfiguration thymeleafAutoConfiguration = new ThymeleafAutoConfiguration();
+//        registry.
+//    }
 //}
